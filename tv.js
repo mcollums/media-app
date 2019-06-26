2a0179d0dacb0b94d1c8388ffe2311c0b2c6f62f
mcollums <collumsmichelle@gmail.com>
2019-06-26 13:21:31 -0500

    starting off the TV function

---
+++ tv.js
@@ -0,0 +1,19 @@
+var axios = require('axios');
+
+
+var TV = function() {
+    this.findShow = function(show) {
+      // The following URL can be used to search the TV Maze API for a given show
+      var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + show;
+
+      ajax.
+  
+    };
+  };
+  
+
+
+  var tv = new TV();
+  TV.findShow("Dexter");
+
+  module.exports = TV;
